aafaf18649cd49198d3c78344ad5530fd988cd9f
Mugen <52742495+eyimofe-olasoko@users.noreply.github.com>
2026-01-13 19:31:11 +0000

    Git Commit

---
+++ Source/TheLookout/TheLookoutCameraManager.cpp
@@ -0,0 +1,11 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "TheLookoutCameraManager.h"
+
+ATheLookoutCameraManager::ATheLookoutCameraManager()
+{
+	// set the min/max pitch
+	ViewPitchMin = -70.0f;
+	ViewPitchMax = 80.0f;
+}
---
+++ Source/TheLookout/TheLookoutGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TheLookoutGameMode.h"
+
+ATheLookoutGameMode::ATheLookoutGameMode()
+{
+	// stub
+}
