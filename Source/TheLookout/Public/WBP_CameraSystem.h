f987afe23b56b1a54d7532d8b86a938141ceab0e
Mugen <52742495+eyimofe-olasoko@users.noreply.github.com>
2026-01-14 22:56:50 +0000

    Adding camera switching widget

---
+++ Source/TheLookout/Public/WBP_CameraSystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "WBP_CameraSystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THELOOKOUT_API UWBP_CameraSystem : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
